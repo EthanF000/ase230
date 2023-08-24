26a08b6c8377cda4420364e823005ee9eed8d0b8
Ethan <etfo225@gmail.com>
2023-08-24 17:30:38 -0400

    Final update - fixed plain HTML cmds

--- Ethan_Fossmeyer.php
+++ Ethan_Fossmeyer.php
@@ -11,6 +11,12 @@
 
 	$summary = 'Experienced technology worker with a strong foundation in IT support, seeking entry level position in Software Development/Engineering. Possesses a proven track record of top-notch technical assistance to high priority users, looking now to provide analytical thinking, problem-solving skills and determination to the realm of software development. Loves to play basketball, play video games, weightlift, collect trading cards, and hike/travel. Has a 15-year old black Dachshund named Indy.';
 
+	$workTitle1 = 'Help Desk Technician';
+	$workSummary1  = 'Providing excellent enterprise IT support to executive customers as well as being the lead of ActiveDirectory account creations and maintenance. Regularly involved in typical and atypical software and hardware problem-solving endeavors.';
+	$workKeyAchievement = 'Designed and implemented a documentation flow using Power Automate, resulting in a measured 30% boost to overall worker productivity.';
+	$workTitle2 = 'Museum Host';
+	$workSummary2 = 'Serving thousands of guests daily out of the many food venues that the Creation Museum owns and operates. Regular top-performer in employee efficiency metrics such as: positive guest interactions, least difference in register cashout tills, and employee of the month awards.';
+	$workCompany = 'Answers in Genesis';
 ?>
 
 <!DOCTYPE html>
@@ -70,7 +76,7 @@
 		    </header>
 		    <div class="resume-body p-5">
 			    <section class="resume-section summary-section mb-5">
-				    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Summary</h2>
+				    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3"><?php echo 'Summary'?></h2>
 				    <div class="resume-section-content">
 					    <p class="mb-0"><?php echo $summary ?></p>
 				    </div>
@@ -78,32 +84,32 @@
 			    <div class="row">
 				    <div class="col-lg-9">
 					    <section class="resume-section experience-section mb-5">
-						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Work Experience</h2>
+						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3"><?php echo 'Work Experience'?></h2>
 						    <div class="resume-section-content">
 							    <div class="resume-timeline position-relative">
 								    <article class="resume-timeline-item position-relative pb-5">
 									    
 									    <div class="resume-timeline-item-header mb-2">
 										    <div class="d-flex flex-column flex-md-row">
-										        <h3 class="resume-position-title font-weight-bold mb-1">Help Desk Technician</h3>
-										        <div class="resume-company-name ms-auto">Answers in Genesis</div>
+										        <h3 class="resume-position-title font-weight-bold mb-1"><?php echo $workTitle1 ?></h3>
+										        <div class="resume-company-name ms-auto"><?php echo $workCompany ?></div>
 										    </div><!--//row-->
-										    <div class="resume-position-time">2022 - Present</div>
+										    <div class="resume-position-time"><?php echo '2022 - Present' ?></div>
 									    </div><!--//resume-timeline-item-header-->
 									    <div class="resume-timeline-item-desc">
-										    <p>Providing excellent enterprise IT support to executive customers as well as being the lead of ActiveDirectory account creations and maintenance. Regularly involved in typical and atypical software and hardware problem-solving endeavors.</p>
-										    <h4 class="resume-timeline-item-desc-heading font-weight-bold">Key Achievement:</h4>
+										    <p><?php echo $workSummary1 ?></p>
+										    <h4 class="resume-timeline-item-desc-heading font-weight-bold"><?php echo 'Key Achievement:'?></h4>
 										    <ul>
-											    <li>Designed and implemented a documentation flow using Power Automate, resulting in a measured 30% boost to overall worker productivity.</li>
+											    <li><?php echo $workKeyAchievement ?></li>
 										    </ul>
-										    <h4 class="resume-timeline-item-desc-heading font-weight-bold">Technologies used:</h4>
+										    <h4 class="resume-timeline-item-desc-heading font-weight-bold"><?php echo 'Technologies used:'?></h4>
 										    <ul class="list-inline">
-											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill">Windows/Linux/Mac</span></li>
-											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill">Power Automate</span></li>
-											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill">PowerShell</span></li>
-											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill">ActiveDirectory</span></li>
-											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill">ZenDesk</span></li>
-											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill">ManageEngine</span></li>
+											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill"><?php echo 'Windows/Linux/Mac'?></span></li>
+											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill"><?php echo 'Power Automate'?></span></li>
+											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill"><?php echo 'PowerShell'?></span></li>
+											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill"><?php echo 'ActiveDirectory'?></span></li>
+											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill"><?php echo 'ZenDesk'?></span></li>
+											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill"><?php echo 'ManageEngine'?></span></li>
 										    </ul>
 									    </div><!--//resume-timeline-item-desc-->
 
@@ -113,18 +119,18 @@
 									    
 									    <div class="resume-timeline-item-header mb-2">
 										    <div class="d-flex flex-column flex-md-row">
-										        <h3 class="resume-position-title font-weight-bold mb-1">Museum Host</h3>
-										        <div class="resume-company-name ms-auto">Answers in Genesis</div>
+										        <h3 class="resume-position-title font-weight-bold mb-1"><?php echo $workTitle2 ?></h3>
+										        <div class="resume-company-name ms-auto"><?php echo $workCompany ?></div>
 										    </div><!--//row-->
-										    <div class="resume-position-time">2016 - 2022</div>
+										    <div class="resume-position-time"><?php echo '2016 - 2022'?></div>
 									    </div><!--//resume-timeline-item-header-->
 									    <div class="resume-timeline-item-desc">
-										    <p>Serving thousands of guests daily out of the many food venues that the Creation Museum owns and operates. Regular top-performer in employee efficiency metrics such as: positive guest interactions, least difference in register cashout tills, and employee of the month awards.</p>
-										    <h4 class="resume-timeline-item-desc-heading font-weight-bold">Technologies used:</h4>
+										    <p><?php echo $workSummary2 ?></p>
+										    <h4 class="resume-timeline-item-desc-heading font-weight-bold"><?php echo 'Technologies used:'?></h4>
 										    <ul class="list-inline">
-											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill">Galaxy</span></li>
-											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill">Revel</span></li>
-											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill">Windows</span></li>
+											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill"><?php echo 'Galaxy'?></span></li>
+											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill"><?php echo 'Revel'?></span></li>
+											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill"><?php echo 'Windows'?></span></li>
 										    </ul>
 									    </div><!--//resume-timeline-item-desc-->
 								    </article><!--//resume-timeline-item-->
@@ -139,19 +145,19 @@
 						        <div class="resume-skill-item">
 							        <ul class="list-unstyled mb-4">
 								        <li class="mb-2">
-								            <div class="resume-skill-name">Java</div>
+								            <div class="resume-skill-name"><?php echo 'Java'?></div>
 									        <div class="progress resume-progress">
 											    <div class="progress-bar theme-progress-bar-dark" role="progressbar" style="width: 55%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
 											</div>
 								        </li>
 								        <li class="mb-2">
-								            <div class="resume-skill-name">Python</div>
+								            <div class="resume-skill-name"><?php echo 'Python'?></div>
 									        <div class="progress resume-progress">
 											    <div class="progress-bar theme-progress-bar-dark" role="progressbar" style="width: 58%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
 											</div>
 								        </li>
 								        <li class="mb-2">
-								            <div class="resume-skill-name">HTML/CSS</div>
+								            <div class="resume-skill-name"><?php echo 'HTML/CSS'?></div>
 									        <div class="progress resume-progress">
 											    <div class="progress-bar theme-progress-bar-dark" role="progressbar" style="width: 50%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
 											</div>
@@ -159,61 +165,61 @@
 							        </ul>
 						        </div><!--//resume-skill-item-->
 						        <div class="resume-skill-item">
-						            <h4 class="resume-skills-cat font-weight-bold">Others</h4>
+						            <h4 class="resume-skills-cat font-weight-bold"><?php echo 'Others'?></h4>
 						            <ul class="list-inline">
-							            <li class="list-inline-item"><span class="badge badge-light">Code Review</span></li>
-							            <li class="list-inline-item"><span class="badge badge-light">Git</span></li>
-							            <li class="list-inline-item"><span class="badge badge-light">VSCode</span></li>
-							            <li class="list-inline-item"><span class="badge badge-light">IntelliJ</span></li>
+							            <li class="list-inline-item"><span class="badge badge-light"><?php echo 'Code Review'?></span></li>
+							            <li class="list-inline-item"><span class="badge badge-light"><?php echo 'Git'?></span></li>
+							            <li class="list-inline-item"><span class="badge badge-light"><?php echo 'VSCode'?></span></li>
+							            <li class="list-inline-item"><span class="badge badge-light"><?php echo 'IntelliJ IDEA'?></span></li>
 						            </ul>
 						        </div><!--//resume-skill-item-->
 						    </div><!--resume-section-content-->
 					    </section><!--//skills-section-->
 					    <section class="resume-section education-section mb-5">
-						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Education</h2>
+						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3"><?php echo 'Education'?></h2>
 						    <div class="resume-section-content">
 							    <ul class="list-unstyled">
 								    <li class="mb-2">
-								        <div class="resume-degree font-weight-bold">BSc in Computer Information Technology</div>
-										<div class="resume-degree-org">with a Minor in Finance</div>
-								        <div class="resume-degree-org">Northern Kentucky University</div>
-								        <div class="resume-degree-time">2020 - 2024</div>
+								        <div class="resume-degree font-weight-bold"><?php echo 'BSc in Computer Information Technology'?></div>
+										<div class="resume-degree-org"><?php echo 'with a Minor in Finance'?></div>
+								        <div class="resume-degree-org"><?php echo 'Northern Kentucky University'?></div>
+								        <div class="resume-degree-time"><?php echo '2020 - 2024'?></div>
 								    </li>
 							    </ul>
 						    </div>
 					    </section><!--//education-section-->
 					    <section class="resume-section reference-section mb-5">
-						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Awards</h2>
+						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3"><?php echo 'Awards'?></h2>
 						    <div class="resume-section-content">
 							    <ul class="list-unstyled resume-awards-list">
 								    <li class="mb-2 ps-4 position-relative">
 								        <i class="resume-award-icon fas fa-trophy position-absolute" data-fa-transform="shrink-2"></i>
-								        <div class="resume-award-name">President's Honor List</div>
-								        <div class="resume-award-desc">Spring 2021, Fall 2021, and Spring 2022. For receiving a GPA of 4.0 in a semester.</div>
+								        <div class="resume-award-name"><?php echo 'President\'s Honor List'?></div>
+								        <div class="resume-award-desc"><?php echo 'Spring 2021, Fall 2021, and Spring 2022. For receiving a GPA of 4.0 in a semester.'?></div>
 								    </li>
 								    <li class="mb-0 ps-4 position-relative">
 								        <i class="resume-award-icon fas fa-trophy position-absolute" data-fa-transform="shrink-2"></i>
-								        <div class="resume-award-name">Dean's List</div>
-								        <div class="resume-award-desc">Fall 2020, Fall 2022, and Spring 2023. For receiving a GPA of 3.5-3.99 in a semester.</div>
+								        <div class="resume-award-name"><?php echo 'Dean\'s List'?></div>
+								        <div class="resume-award-desc"><?php echo 'Fall 2020, Fall 2022, and Spring 2023. For receiving a GPA of 3.5-3.99 in a semester.'?></div>
 								    </li>
 							    </ul>
 						    </div>
 					    </section><!--//interests-section-->
 					    <section class="resume-section language-section mb-5">
-						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Languages</h2>
+						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3"><?php echo 'Languages'?></h2>
 						    <div class="resume-section-content">
 							    <ul class="list-unstyled resume-lang-list">
-								    <li class="mb-2"><span class="resume-lang-name font-weight-bold">English</span> <small class="text-muted font-weight-normal">(Native)</small></li>
+								    <li class="mb-2"><span class="resume-lang-name font-weight-bold"><?php echo 'English'?></span> <small class="text-muted font-weight-normal"><?php echo '(Native)'?></small></li>
 							    </ul>
 						    </div>
 					    </section><!--//language-section-->
 					    <section class="resume-section interests-section mb-5">
-						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Interests</h2>
+						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3"><?php echo 'Interests'?></h2>
 						    <div class="resume-section-content">
 							    <ul class="list-unstyled">
-								    <li class="mb-1">Weightlifting</li>
-								    <li class="mb-1">Basketball</li>
-								    <li class="mb-1">Hiking</li>
+								    <li class="mb-1"><?php echo 'Weightlifting'?></li>
+								    <li class="mb-1"><?php echo 'Basketball'?></li>
+								    <li class="mb-1"><?php echo 'Hiking'?></li>
 							    </ul>
 						    </div>
 					    </section><!--//interests-section-->
@@ -221,15 +227,15 @@
 				    </div>
 			    </div><!--//row-->
 				<section class="resume-section experience-section mb-5">
-					<h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Projects</h2>
+					<h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3"><?php echo 'Projects'?></h2>
 					<div class="row mt-4">
 						<div class="col-md-4">
 							<div class="card">
 								<img src="path-to-project-image1.jpg" alt="Resume Project" class="card-img-top">
 								<div class="card-body">
-									<h5 class="card-title">PHP Resume</h5>
-									<p class="card-text">Resume created using PHP.</p>
-									<a href="btn btn-outline-primary" href="#">https://github.com/EthanF000/ase230</a>
+									<h5 class="card-title"><?php echo 'PHP Resume'?>e</h5>
+									<p class="card-text"><?php echo 'Resume created using PHP.'?></p>
+									<a href="btn btn-outline-primary" href="#"><?php echo 'https://github.com/EthanF000/ase230'?></a>
 								</div>
 							</div>
 						</div>
@@ -244,7 +250,7 @@
     
     <footer class="footer text-center pt-2 pb-5">
 	    <!--/* This template is free as long as you keep the footer attribution link. If you'd like to use the template without the attribution link, you can buy the commercial license via our website: themes.3rdwavemedia.com Thank you for your support. :) */-->
-        <small class="copyright">Designed with <span class="sr-only">love</span><i class="fas fa-heart"></i> by Your name</small>
+        <small class="copyright">Designed with <span class="sr-only">love</span><i class="fas fa-heart"></i> <?php echo $name ?></small>
     </footer>
 
     
